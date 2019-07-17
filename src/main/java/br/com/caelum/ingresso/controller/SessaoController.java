eaaa6b871cd573e90b6edde79090576bdb72a352
Filipe Alfredo <filipealfredo.dev@gmail.com>
2019-07-16 22:48:46 -0300

    fim de aula

--- src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
+++ src/main/java/br/com/caelum/ingresso/controller/SessaoController.java
@@ -2,7 +2,7 @@ package br.com.caelum.ingresso.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -18,7 +18,7 @@ public class SessaoController {
 	@Autowired
 	private FilmeDao filmeDao;
 	
-	@GetMapping("/admin/sessao")
+	@RequestMapping("/admin/sessao")
 	public ModelAndView form(@RequestParam("salaId") Integer salaId) {
 
 		ModelAndView modelAndView = new ModelAndView("sessao/sessao");
