6675896b58c23387e704933ce5f23c2d5af5346b
Filipe Alfredo <filipealfredo.dev@gmail.com>
2019-07-18 22:25:31 -0300

    Exercicio 2.12

--- src/main/java/br/com/caelum/ingresso/model/Sessao.java
+++ src/main/java/br/com/caelum/ingresso/model/Sessao.java
@@ -1,5 +1,7 @@
 package br.com.caelum.ingresso.model;
 
+import java.math.BigDecimal;
+import java.math.RoundingMode;
 import java.time.LocalTime;
 
 import javax.persistence.Entity;
@@ -15,6 +17,7 @@ public class Sessao {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Integer id;
 	private LocalTime horario;
+	private BigDecimal preco;
 	
 	@ManyToOne
 	private Sala sala;
@@ -23,13 +26,13 @@ public class Sessao {
 	private Filme filme;
 	
 	public Sessao() {
-		
 	}
 	
 	public Sessao(LocalTime horario, Filme filme, Sala sala) {
 		this.horario = horario;
 		this.filme = filme;
 		this.sala = sala;
+		this.preco = sala.getPreco().add(filme.getPreco());
 	}
 
 	public Integer getId() {
@@ -62,5 +65,13 @@ public class Sessao {
 
 	public void setFilme(Filme filme) {
 		this.filme = filme;
+	}
+	
+	public BigDecimal getPreco() {
+		return preco.setScale(2, RoundingMode.HALF_UP);
+	}
+	
+	public void setPreco(BigDecimal preco) {
+		this.preco = preco;
 	}
 }
